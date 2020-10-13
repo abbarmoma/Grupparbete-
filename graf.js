f33126d41886c41fcbe802de62d6b3ddfc34f102
Sebastian <sebb302@gmail.com>
2020-10-13 14:29:00 +0200

    förbätrrade grafer

--- graf.js
+++ graf.js
@@ -1,40 +1,37 @@
-google.charts.load('current', {'packages':['line']});
-      google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawBasic);
 
-    function drawChart() {
+function drawBasic() {
 
       var data = new google.visualization.DataTable();
-      data.addColumn('number', 'Day');
-      data.addColumn('number', 'Terrariet');
-      data.addColumn('number', 'Kafeterian');
-      data.addColumn('number', 'Biblioteket');
+      data.addColumn('number', 'Tid');
+      data.addColumn('number', 'Temperatur');
 
       data.addRows([
-        [1,  37.8, 80.8, 41.8],
-        [2,  30.9, 69.5, 32.4],
-        [3,  25.4,   57, 25.7],
-        [4,  11.7, 18.8, 10.5],
-        [5,  11.9, 17.6, 10.4],
-        [6,   8.8, 13.6,  7.7],
-        [7,   7.6, 12.3,  9.6],
-        [8,  12.3, 29.2, 10.6],
-        [9,  16.9, 42.9, 14.8],
-        [10, 12.8, 30.9, 11.6],
-        [11,  5.3,  7.9,  4.7],
-        [12,  6.6,  8.4,  5.2],
-        [13,  4.8,  6.3,  3.6],
-        [14,  4.2,  6.2,  3.4]
-
+        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+        [66, 70], [67, 72], [68, 75], [69, 80]
       ]);
 
       var options = {
-        chart: {
-          title: 'Temperaturen under senaste vecka:'
+        hAxis: {
+          title: 'Tid'
         },
-       
+        vAxis: {
+          title: 'temperatur'
+        }
       };
 
-      var chart = new google.charts.Line(document.getElementById('container'));
+      var chart = new google.visualization.LineChart(document.getElementById('container'));
 
-      chart.draw(data, google.charts.Line.convertOptions(options));
+      chart.draw(data, options);
     }
